09f34161c6cb07980a8039c180c1692716f99a0e
jjmerri <jjmerri88@gmail.com>
2022-05-29 17:43:51 -0400

    proto commit -jjm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const getCombos = () => {
+  const combos = [];
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      combos.push({
+        color1: "black",
+        number1: i + 1,
+        color2: "black",
+        number2: j + 1,
+        id: combos.length + 1,
+      });
+    }
+  }
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      combos.push({
+        color1: "black",
+        number1: i + 1,
+        color2: "red",
+        number2: j + 1,
+        id: combos.length + 1,
+      });
+    }
+  }
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      combos.push({
+        color1: "red",
+        number1: i + 1,
+        color2: "black",
+        number2: j + 1,
+        id: combos.length + 1,
+      });
+    }
+  }
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      combos.push({
+        color1: "red",
+        number1: i + 1,
+        color2: "red",
+        number2: j + 1,
+        id: combos.length + 1,
+      });
+    }
+  }
+  return combos;
+};
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+const combos = getCombos();
 
 function App() {
+  const [comboHistory, setComboHistory] = useState([]);
+
+  const getCombo = () => {
+    setComboHistory([...comboHistory, combos[getRandomInt(1, 256)]]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getCombo}>Random Combo</button>
+
+      {comboHistory
+        ?.map((combo) => {
+          return (
+            <p>{`${combo.id} - ${combo.color1}:${combo.number1} - ${combo.color2}:${combo.number2}`}</p>
+          );
+        })
+        .reverse()}
     </div>
   );
 }
