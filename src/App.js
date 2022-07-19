a73ca5119bdd3c71c4a0f034347bf52c8a1f7a72
jjmerri <jjmerri88@gmail.com>
2022-07-18 20:46:19 -0400

    add filter -jjm

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const getCombos = () => {
   return combos;
 };
 
-function shuffle(original) {
+const shuffle = (original) => {
   const array = [...original];
   let currentIndex = array.length,
     randomIndex;
@@ -43,13 +43,19 @@ function shuffle(original) {
   }
 
   return array;
-}
+};
 
 const combos = getCombos();
 
 function App() {
   const [shuffledHistory, setShuffledHistory] = useState([combos]);
   const [historyIndex, setHistoryIndex] = useState(0);
+  const [filterVal, setFilterVal] = useState({
+    color1: "black",
+    number1: 4,
+    color2: "red",
+    number2: 5,
+  });
 
   const shuffleCombos = useCallback(() => {
     setHistoryIndex(shuffledHistory.length);
@@ -57,11 +63,49 @@ function App() {
     setShuffledHistory((prev) => [...prev, shuffled]);
   }, [shuffledHistory]);
 
+  const calcFilterVal = (val) => {
+    var comboRegEx = /^([BR])([1-8])([BR])([1-8])$/g;
+    var match = comboRegEx.exec(val.toUpperCase());
+
+    if (match) {
+      const color1 = match[1] === "B" ? "black" : "red";
+      const number1 = parseInt(match[2]);
+      const color2 = match[3] === "B" ? "black" : "red";
+      const number2 = parseInt(match[4]);
+
+      setFilterVal({ color1, number1, color2, number2 });
+    } else {
+      setFilterVal({});
+    }
+  };
+
   return (
     <div className="App">
       <button onClick={shuffleCombos} style={{ marginTop: "1rem" }}>
         Shuffle Combos
       </button>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
+      >
+        <label>
+          {"Find: "}
+          <input
+            placeholder="B4R5"
+            style={{ marginRight: "1rem" }}
+            onChange={(e) => calcFilterVal(e.target.value)}
+            defaultValue="B4R5"
+          />
+        </label>
+        <label>
+          {shuffledHistory[historyIndex].findIndex(
+            (combo) =>
+              combo.color1 === filterVal.color1 &&
+              combo.number1 === filterVal.number1 &&
+              combo.color2 === filterVal.color2 &&
+              combo.number2 === filterVal.number2
+          ) + 1}
+        </label>
+      </div>
       <div
         style={{ display: "flex", justifyContent: "center", marginTop: "1rem" }}
       >
